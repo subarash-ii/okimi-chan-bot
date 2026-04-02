caa91cb19f6ac55d5c3a9f19269c99ac0b1217ed
Maksym-Ryaboshapka <testbs777888@gmail.com>
2026-04-02 19:57:17 +0300

    change docker-compose.yml and username in start msg

--- src/bot/commands/start.ts
+++ src/bot/commands/start.ts
@@ -17,7 +17,7 @@ export default async function onStart(bot: TelegramBot, msg: Message): Promise<v
 Для полного списка команд и дополнительной информации используйте /help
 
 *🛠️ Разработчики:*
-@HELLPER7788 • @ByteMe6 • @ArchiPank
+@Subarash_ii • @ByteMe6 • @ArchiPank
 `;
 
   await bot.sendSticker(msg.chat.id, "CAACAgIAAxkBAAE-NAVpIb_kUswE4rTMPRRfSjvOTsXS_AACZ4EAAulSIUtSawUNN0GDCzYE");
